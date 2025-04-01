72df99c9d2810d4b854fd7ce2e93d8668490ce96
WACHIDADUCK <erguile@hotmail.com>
2025-04-01 09:09:43 +0100

    fix(backend/faker/lessonFactory): ajusted time to be 1-2 hours diference from the start_time

--- backend/app/v1/repositories/fakers/factories/lessonFactory.py
+++ backend/app/v1/repositories/fakers/factories/lessonFactory.py
@@ -3,6 +3,8 @@ from faker import Faker
 from sqlalchemy.sql.expression import func
 from app.v1.models.lesson import Lesson
 import random
+from datetime import timedelta
+
 fake = Faker()
 
 class LessonFactory(factory.Factory):
@@ -10,8 +12,12 @@ class LessonFactory(factory.Factory):
         model = Lesson
 
     max_capacity = factory.Faker('random_int', min=8, max=15)
-    # Fecha de inicio: desde hoy hasta 30 días en el futuro
     start_time = factory.Faker('date_time_between', start_date='now', end_date='+30d')
-    # Fecha de fin: 1 o 2 horas después de start_time
-    end_time = factory.Faker('date_time_between', start_date=start_time, end_date=f'+{random.randint(1,2)}h')
+    
+    @factory.lazy_attribute
+    def end_time(self):
+        # Add 1 or 2 hours to start_time
+        hours_to_add = random.randint(1, 2)
+        return self.start_time + timedelta(hours=hours_to_add)
+    
     lesson_url = factory.Faker('image_url')
