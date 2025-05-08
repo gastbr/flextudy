6886255eba5a1b3d8438038cdbcd8d8e6721491d
Gastón <gaston@tpz>
2025-05-08 12:07:04 +0100

    feat(frontend/layout): implement hoverable profile pic to show additional user info

--- frontend/app/dashboard/layout.tsx
+++ frontend/app/dashboard/layout.tsx
@@ -7,6 +7,7 @@ import { Calendar, Users, Wallet, Settings, User, LogOut, Menu, GraduationCap } 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
 import { useProvider } from '@/app/context/provider'
+import { Tooltip, TooltipTrigger, TooltipContent, TooltipProvider } from "@/components/ui/tooltip";
 
 interface DashboardLayoutProps {
   children: React.ReactNode
@@ -68,15 +69,30 @@ function DashboardHeader({ user }: { user: any }) {
           </Button>
 
           <div className="flex items-center gap-2">
-            <Avatar className="h-8 w-8">
-              <AvatarImage src={user?.profile_pic} alt="User" />
-              <AvatarFallback>JD</AvatarFallback>
-            </Avatar>
-            <div className="hidden md:block">
-              <div className="text-sm font-medium">{user?.name}</div>
-              <div className="text-xs text-muted-foreground">{user?.user_type_name.toUpperCase()}</div>
-            </div>
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Link href="/dashboard/profile/me" className="flex items-center gap-2 hover:bg-muted p-2 rounded-md">
+                    <Avatar className="h-8 w-8">
+                      <AvatarImage src={user?.profile_pic} alt="User" />
+                      <AvatarFallback>JD</AvatarFallback>
+                    </Avatar>
+                    <div className="hidden md:block">
+                      <div className="text-sm font-medium">{user?.username}</div>
+                      <div className="text-xs text-muted-foreground">{user?.user_type_name.toUpperCase()}</div>
+                    </div>
+                  </Link>
+                </TooltipTrigger>
+                <TooltipContent>
+                  <div className="text-xs round-sm ">
+                    <p><strong>Name:</strong> {user?.name}</p>
+                    <p><strong>Email:</strong> {user?.email}</p>
+                  </div>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
           </div>
+
         </div>
       </div>
     </header>
