7f8310ff1128e699d2d1788a6256336e888795cd
Gastón <gaston@tpz>
2025-05-14 13:38:53 +0100

    feat(backend): implement attendance routes for students to enroll and cancel lessons enrollment

--- backend/app/v1/models/attend.py
+++ backend/app/v1/models/attend.py
@@ -1,18 +1,16 @@
 from sqlmodel import SQLModel, Field, Relationship
 from typing import Optional
 
-
 class BaseAttend(SQLModel):
     assistance: bool
+    lesson_id: int
 
 class Attend(BaseAttend, table=True):
-    student_id: Optional[int] | None = Field(default=None, foreign_key="user.id", primary_key=True)
     lesson_id: Optional[int] | None = Field(default=None, foreign_key="lesson.id", primary_key=True)
+    student_id: Optional[int] | None = Field(default=None, foreign_key="user.id", primary_key=True)
 
 class CreateAttend(BaseAttend):
     assistance: Optional[bool] = False
-    lesson_id: Optional[int] = None
-    student_id: Optional[int] = None
 
 class UpdateAttend(BaseAttend):
     assistance: Optional[bool] = None
--- backend/app/v1/routes/attend_router.py
+++ backend/app/v1/routes/attend_router.py
@@ -1,12 +1,14 @@
 # routes/example_route.py
 from fastapi import APIRouter, Depends,HTTPException
-from app.v1.models.attend import Attend
+from app.v1.models.attend import Attend, CreateAttend, ReadAttend
 from sqlmodel.ext.asyncio.session import AsyncSession
 from typing import List, Annotated
-from app.v1.services.auth.auth_service import authorize
+from app.v1.services.auth.auth_service import authorize, authorize_roles
 from app.v1.services.attend_service import (
     get_attends_by_id_lesson,
-    update_class_attends
+    update_class_attends,
+    create_class_attends,
+    delete_attendance
 )
 from app.config.db import get_session
 
@@ -22,6 +24,17 @@ async def show_students_attends_by_lesson_id(
         raise HTTPException(status_code=404, detail="Class not found")
     return attends
 
+@router.post("/{lesson_id}", response_model=ReadAttend)
+async def add_authenticated_student_enrollment(
+    user: Annotated[None, Depends(authorize)],
+    lesson_id: int,
+    session: AsyncSession = Depends(get_session)):
+    authorize_roles(user, ["student"])  
+    attends =  await create_class_attends(session, lesson_id, user)
+    if not attends:
+        raise HTTPException(status_code=404, detail="Class not found")
+    return attends 
+
 @router.put("/class_attends/{lesson_id}", response_model=dict)
 async def update_the_attends_of_a_lesson(
     user: Annotated[None, Depends(authorize)],
@@ -33,3 +46,13 @@ async def update_the_attends_of_a_lesson(
         raise HTTPException(status_code=404, detail="Class not found")
     return attends
 
+@router.delete("/{lesson_id}", response_model=ReadAttend)
+async def delete_authenticated_student_enrollment_from_lesson(
+    user: Annotated[None, Depends(authorize)],
+    lesson_id: int,
+    session: AsyncSession = Depends(get_session)):
+    authorize_roles(user, ["student"])  
+    attends =  await delete_attendance(session, lesson_id, user)
+    if ValueError:
+        raise HTTPException(status_code=404, detail="Class not found")
+    return attends
